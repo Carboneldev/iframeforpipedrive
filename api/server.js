786cb09873d1805ccb25de75d628bd36a1008643
Carboneldev <carbonelnata@gmail.com>
2024-05-23 22:41:57 +0200

    Server add

--- api/server.js
+++ api/server.js
@@ -14,9 +14,9 @@ app.get('/pipedrive/callback', async (req, res) => {
         const response = await axios.post('https://oauth.pipedrive.com/oauth/token', {
             grant_type: 'authorization_code',
             code: code,
-            redirect_uri: 'https://iframe-pipedrive.vercel.app/', // Замените на ваш фактический Callback URL
-            client_id: '9eb24d108673c154', // Замените на ваш фактический Client ID
-            client_secret: '3d1be9d537a426631b267bfb0c6a5191155f0d73' // Замените на ваш фактический Client Secret
+            redirect_uri: 'https://1d31-2a01-e0a-a9e-9f30-adfe-4bb0-66ea-60b.ngrok-free.app/pipedrive/callback', // Обновите на текущий ngrok URL
+            client_id: '9eb24d108673c154', // Ваш фактический Client ID
+            client_secret: '3d1be9d537a426631b267bfb0c6a5191155f0d73' // Ваш фактический Client Secret
         });
 
         const { access_token } = response.data;
@@ -26,7 +26,19 @@ app.get('/pipedrive/callback', async (req, res) => {
 
         res.send('Authorization successful! You can close this tab.');
     } catch (error) {
-        console.error('Error exchanging authorization code for access token:', error);
+        if (error.response) {
+            // Сервер ответил статусом, отличным от 2xx
+            console.error('Error response data:', error.response.data);
+            console.error('Error response status:', error.response.status);
+            console.error('Error response headers:', error.response.headers);
+        } else if (error.request) {
+            // Запрос был сделан, но ответа не получено
+            console.error('Error request data:', error.request);
+        } else {
+            // Произошла ошибка в настройке запроса
+            console.error('Error message:', error.message);
+        }
+        console.error('Error config:', error.config);
         res.status(500).send('An error occurred during the authorization process.');
     }
 });
